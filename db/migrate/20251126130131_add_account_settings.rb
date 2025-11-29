559629537b2fb93a6d27a0bf22d115de68dacff3
David Heinemeier Hansson <david@hey.com>
2025-11-29 10:52:01 -0800

    We don't need to specify the default any more

--- db/migrate/20251126130131_add_account_settings.rb
+++ db/migrate/20251126130131_add_account_settings.rb
@@ -1,5 +1,5 @@
 class AddAccountSettings < ActiveRecord::Migration[7.2]
   def change
-    add_column :accounts, :settings, :json, default: {}
+    add_column :accounts, :settings, :json
   end
 end

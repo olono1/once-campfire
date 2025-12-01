3367ffaf8fbd8164f02d0c8e920414a202326d87
Stanko K.R. <stanko@stanko.io>
2025-12-01 11:47:51 +0100

    Switch to using schema.rb
    Previously we had to use structure.sql since schema.rb didn't have support for virtual tables that we needed for search. Since Campfire's release virtuals tables have been added to Rails, so there is no need to use structure.sql anymore

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,5 @@ module Campfire
 
     # Fallback to English if translation key is missing
     config.i18n.fallbacks = true
-
-    # Use SQL schema format to include search-related objects
-    config.active_record.schema_format = :sql
   end
 end
